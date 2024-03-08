751c36fe150477af85d14535ca32e18ee4554a6d
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-08 19:47:01 +0200

    Dealing with modifiers

---
+++ app/src/main/java/com/khumaloaugustine/simpletextapplication/Modifiers.kt
@@ -0,0 +1,18 @@
+package com.khumaloaugustine.simpletextapplication
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun Modifiers() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.LightGray)) {
+
+    }
+}
