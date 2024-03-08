9c14a7c59ce055bcfa3c401ada56dcf0cf9cb8d6
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-08 19:52:56 +0200

    PLACED THE TEXT AT THE CENTER OF THE SCREEN

--- app/src/main/java/com/khumaloaugustine/simpletextapplication/Modifiers.kt
+++ app/src/main/java/com/khumaloaugustine/simpletextapplication/Modifiers.kt
@@ -1,17 +1,21 @@
 package com.khumaloaugustine.simpletextapplication
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Modifiers() {
-    Column(modifier = Modifier
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
         .fillMaxSize()
         .background(Color.LightGray)) {
         Text(text = "Welcome to my composable project")
